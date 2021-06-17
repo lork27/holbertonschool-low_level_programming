ec03671298d8420d4a37e04039025544c106f1ed
lork27 <g.lorca.lamadrid@gmail.com>
2021-06-17 17:32:44 +0000

    C program that prints something with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - assigns a string variable and pass it to print top print it out
+ *Return: 0
+ */
+
+int main(void)
+{
+	char *MyStr = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf("%s", MyStr);
+	return (0);
+}
