e2a43b7cce0a3955d5202694953b2d37cd50f17a
lork27 <g.lorca.lamadrid@gmail.com>
2021-06-17 17:09:40 +0000

    no comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
-*main - prints string string using puts
+*main - prints string string using puts function
 *Return: 0
 */
 int main(void)
 {
 
-	char *string = ""Programming is like building a multilingual puzzle\n";
+	char *string = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(string);
 	return (0);
