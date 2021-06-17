d7398374c7c5b99bfa58513b212045dacfc5af05
lork27 <g.lorca.lamadrid@gmail.com>
2021-06-17 16:51:21 +0000

    tried creating the string variable in a different way

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,9 @@
 int main(void)
 {
 
-	char MyStr[] = "\"Programming is like building a multilingual puzzle\n";
+	char *string = "\"Programming is like building a multilingual puzzle\n";
 
-	puts(MyStr);
+	puts(string);
 	return (0);
 
 }
