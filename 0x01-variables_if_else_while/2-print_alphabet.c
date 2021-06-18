a3932f5b0754d13db3a7e274d40089e5f436d81f
lork27 <g.lorca.lamadrid@gmail.com>
2021-06-18 23:38:04 +0000

    program that prints the alphabet with putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - function
+*Return: 0 correct
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar (c);
+	}
+	putchar ('\n');
+	return (0);
+}
