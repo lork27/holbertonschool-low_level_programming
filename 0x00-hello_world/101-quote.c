fd17a8784eef493997d59410fdd1bfc0f3dc5d3a
lork27 <g.lorca.lamadrid@gmail.com>
2021-06-18 14:14:17 +0000

    C program that prints something to the stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+*main - The main function in which I print an error message
+*Return: 1
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
