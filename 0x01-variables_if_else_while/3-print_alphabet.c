c4b939e5f29dea1185b82bdb90fb1892f901bbd0
lork27 <g.lorca.lamadrid@gmail.com>
2021-06-18 23:42:11 +0000

    script that prints the alphabet bot in upper and lower case with putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - function
+*Return: 0 correct
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar (c);
+	}
+	for (c = 'A' ; c <= 'Z' ; c++)
+	{
+	putchar ('\n');
+	}
+	return (0);
+}
