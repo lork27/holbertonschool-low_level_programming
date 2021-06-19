b2cd2f68085de8b82b2483c6c016abb758547c4d
lork27 <g.lorca.lamadrid@gmail.com>
2021-06-19 03:50:43 +0000

    program that print all the numbers of base 16 with putchar

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - function
+ *Return: 0 correct
+*/
+int main(void)
+{
+
+int i = 48;
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+char l = 'a';
+	while (l <= 'f')
+	{
+		putchar(l);
+		l++;
+	}
+putchar('\n');
+return (0);
+}
