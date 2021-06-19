c5d6b7a4e3b40cf2af58ee51ce1b3c7ce4e0c24a
lork27 <g.lorca.lamadrid@gmail.com>
2021-06-19 03:34:46 +0000

    changes < to >

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - function
+ *Return: 0 correct
+*/
+int main(void)
+{
+
+char i = 'z';
+	while (i >= 'a')
+	{
+		putchar(i);
+		i--;
+	}
+putchar('\n');
+return (0);
+}
