abddf2d674101bd664bfbecc5c49118187a8f79a
lork27 <g.lorca.lamadrid@gmail.com>
2021-06-22 17:49:52 +0000

    program that checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "holberton.h"
+/**
+ *_islower - checks for lowercase character
+ * Return: 0
+ */
+int _islower(int x)
+{
+	if (islower(x) == 1)
+	{;
+	return (1);
+	else
+	{
+	return (0);
+	+
+}
