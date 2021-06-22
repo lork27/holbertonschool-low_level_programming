dca5183da64a51f5ce18460bb369961a31af6069
lork27 <g.lorca.lamadrid@gmail.com>
2021-06-22 18:37:13 +0000

    function that checks for a letter that is lower or upper case

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *_isalpha - checks for lowercase character
+ * Return: 0
+ * @c: the number we pass the function to compare it
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) && (c 97 >= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
