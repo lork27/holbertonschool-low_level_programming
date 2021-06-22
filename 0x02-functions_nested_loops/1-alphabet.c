3f719245b8de767b415260314177ba78c232dd6e
lork27 <g.lorca.lamadrid@gmail.com>
2021-06-22 17:17:51 +0000

    program that prints the alphabet in lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,29 +1,17 @@
 #include <stdio.h>
-char print_alphabet(void);
-/**
- * main - check the code for Holberton School students.
- * Return: Always 0
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
+#include "holberton.h"
 /**
  * print_alphabet - prints the alphabet with just 2 putchar
  * Return: 0
  */
 
-char print_alphabet(void)
+void print_alphabet(void)
 {
-	char c;
+	int c;
 
 	for (c = 'a' ; c <= 'z' ; c++)
 	{
-		putchar (c);
+		_putchar (c);
 	}
-	putchar ('\n');
-	return (0);
-
+	_putchar ('\n');
 }
