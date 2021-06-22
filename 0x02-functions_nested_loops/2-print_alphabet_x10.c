e3dec01cfdf7febd3f4b051aa544b3b2adf0a525
lork27 <g.lorca.lamadrid@gmail.com>
2021-06-22 17:26:41 +0000

    program that prints 10 times the alphabet, in lower case

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,33 +1,20 @@
 #include <stdio.h>
-char print_alphabet_x10(void);
-/**
- * main - check the code for Holberton School students.
- * Return: Always 0
- */
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
-
+#include "holberton.h"
 /**
  * print_alphabet_x10 - prints the alphabet 10 times with just 2 putchar
  * Return: 0
  */
 
-char print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
-	int reps;
-	char c;
+	int c;
 
-	for (reps = 0; reps <= 9; reps++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (c = 'a' ; c <= 'z' ; c++)
 		{
-		putchar (c);
+			_putchar (c);
 		}
+		_putchar ('\n');
 	}
-	putchar ('\n');
-	return (0);
-
 }
