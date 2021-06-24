64a03ead241e07a0bb18c8f433a789a764beb153
lork27 <g.lorca.lamadrid@gmail.com>
2021-06-24 18:39:11 +0000

    test

---
+++ 0x04-more_functions_nested_loops/isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *_isupper - function that takes an in and checks for uppercase
+ *@c: the int it takes
+ *Return: 1 if upper else 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
