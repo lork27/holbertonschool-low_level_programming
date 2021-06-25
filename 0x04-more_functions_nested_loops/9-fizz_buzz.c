d5ae48c0f29b2d1b331dedae26e7f4835492fa8c
lork27 <g.lorca.lamadrid@gmail.com>
2021-06-25 23:59:17 +0000

    pls work

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,25 +12,20 @@ int main(void)
 		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
-			putchar(' ');
 		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz");
-			putchar(' ');
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
-			putchar(' ');
 		}
 		else
 		{
 			printf("%i ", i);
 		}
-		if (i == 100)
-			continue;
-		putchar(' ';
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
