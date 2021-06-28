ea5369abd81eb9bba20eab09cd5cd9f7e0de8679
lork27 <g.lorca.lamadrid@gmail.com>
2021-06-28 23:17:59 +0000

    function that reverses any given string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *rev_string - prints a given string in reverse
+ *@s: the string we pass
+ */
+void rev_string(char *s)
+{
+	int length, begin;
+	char buf;
+
+	for (length = 0; s[length] != '\0'; length++)
+	for (begin = 0, length--; begin <= length; begin++, length--)
+	{
+		buf = s[begin];
+		s[begin] = s[length];
+		s[length] = buf;
+	}
+}
