7d419985289c02a39bf48940200fe0403627b267
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-07 19:09:58 +0000

    prints string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *_print_rev_recursion - prints string in reverse
+ *@s: the string is going to print
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
