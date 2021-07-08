68edcf950ff75de0cb33f0d11ee348eea1efa539
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-08 19:30:20 +0000

    moved everything to a single loop

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,21 +9,16 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i;
+	int i, j;
 	int square = size * size;
 	int ct, ct2;
 
 
-	for (i = 0; i < square; i += size + 1)
+	for (i = 0, j = size - 1; i < square; i += size + 1, j = j + size - 1)
 	{
 		/*print sum of every point we parsed through*/
 		ct += a[i];
-	}
-
-	for (i = size - 1; i < square; i = i + size - 1)
-	{
-		/*print sum of every point we parsed through*/
-		ct2 += a[i];
+		ct2 += a[j];
 	}
 
 	printf("%i, %i \n", ct, ct2);
