eabbbdcd0c0257ad7345e796449b2289e5e3f9b8
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-08 20:01:40 +0000

    initialized the ct variables

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,17 +11,16 @@ void print_diagsums(int *a, int size)
 {
 	int i, j;
 	int square = size * size;
-	int ct, ct2;
+	int ct = 0, ct2 = 0;
 
 
-	for (i = 0, j = size - 1; i < square; i += size + 1, j = j + size - 1)
+	for (i = 0, j = size - 1; i < square; i += size + 1, j += size - 1)
 	{
-		/*print sum of every point we parsed through*/
 		ct += a[i];
 		ct2 += a[j];
 	}
 
-	printf("%i, %i \n", ct, ct2);
+	printf("%d, %d \n", ct, ct2);
 
 }
 
