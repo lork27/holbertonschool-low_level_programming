ec2d93980ceec97189f202c215971e03814334bd
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-09 16:53:55 +0000

    fixed condition if arg is 1 or 2

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 {
 	int x, y, result;
 
-	if (argc == 1)
+	if (argc < 3)
 	{
 		printf("error\n");
 		return (1);
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 		x = atoi(argv[1]);
 		y = atoi(argv[2]);
 		result = x * y;
-		printf("%d/n", result);
+		printf("%d\n", result);
 		return (0);
 	}
 }
