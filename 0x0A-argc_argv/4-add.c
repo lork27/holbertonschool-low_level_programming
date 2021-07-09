7ae0accee438285486a54deea2d1efde77913e97
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-09 19:22:37 +0000

    print sum of positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,25 +10,30 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
-	if (argc <= 1)
-		printf("0\n");
-
-	else if (argc > 1)
+	if (argc == 1)
 	{
-		for (i = 1; i < argc; i++)
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!isdigit(atoi(argv[i])))
+			if (argv[i][j] <= '0' || argv[i][j] >= '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
-			sum += atoi(argv[i]);
+			else
+			{
+				sum += atoi(argv[i]);
+			}
 		}
-
-		printf("%d\n", sum);
 	}
+	printf("%d\n", sum);
 	return (0);
 }
 
