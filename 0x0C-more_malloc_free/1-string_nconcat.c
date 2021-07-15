4c212fc1662f7fdbb00d7834beb4af650a421dc9
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-15 21:56:52 +0000

    fixed typo on the first loop

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,21 +1,20 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include "holberton.h"
-unsigned int _strlen(char *string);
+unsigned int _strlen(char *str);
 
 
 /**
  *string_nconcat - concat two string
  *@s1: string 1
  *@s2: string 2
- @n: number of bytes of 2 to be concat
+ *@n: number of bytes of 2 to be concat
  *Return: the concatenated string
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s3;
 	unsigned int i, size;
+	char *s3;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -30,14 +29,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 		size += _strlen(s2);
 
-	s3 = malloc(sizeof(char) * size);
+	s3 = malloc(sizeof(char) * size + 1);
 	if (s3 == NULL)
 		return (NULL);
 
-	for (size = 0; s1[size] != '\0'; i++)
-	{
+	for (size = 0; s1[size] != '\0'; size++)
 		s3[size] = s1[size];
-	}
 
 	for (i = 0; s2[i] != '\0' && i < n; i++)
 	{
@@ -50,14 +47,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 /**
  *_strlen - gets string len
- *@string: string to get the length of
+ *@str: string to get the length of
  *Return: length of str as int
  */
 
-unsigned int _strlen(char *string)
+unsigned int _strlen(char *str)
 {
-	unsigned int size;
-	for (size = 0; string[size] != '\0'; size++)
+	unsigned int len;
+
+	for (len = 0; str[len] != '\0'; len++)
 		;
-	return(size);
+	return (len);
 }
