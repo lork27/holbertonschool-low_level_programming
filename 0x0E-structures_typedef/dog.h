8a49e994346fbf2ce591d586f1ad99a20626b830
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-19 19:14:24 +0000

    dog prototype

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifdef DOG_H
+#define DOG_H
+/**
+ *struct dog - dog class
+ *@name: dog name
+ *@age: age of the dog
+ *@owner: dog owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char * owner;
+}
+#endif /*DOG_H*/
