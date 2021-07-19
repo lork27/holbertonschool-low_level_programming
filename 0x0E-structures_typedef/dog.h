953513b000c256ffa4bbeed912b903415479d49e
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-19 19:28:53 +0000

    fixed guard

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,15 @@
-#ifdef DOG_H
-#define DOG_H
+#ifndef DOG
+#define DOG
 /**
  *struct dog - dog class
  *@name: dog name
  *@age: age of the dog
  *@owner: dog owner
- *
  */
 struct dog
 {
 	char *name;
 	float age;
-	char * owner;
-}
-#endif /*DOG_H*/
+	char *owner;
+};
+#endif /*DOG*/
