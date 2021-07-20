4492def711a3a8c42fff48450a9ef01c261b78fd
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-20 02:53:47 +0000

    frees a dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ *new_dog - initializes variable of type struct dog
+ *@name: name of the new dog
+ *@age: age of the dog
+ *@owner: owner of the dog
+ *@return: dog_t
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+}
