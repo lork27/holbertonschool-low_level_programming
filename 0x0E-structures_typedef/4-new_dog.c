b3df1a8801498cee416e947c5055b66f903d6cb7
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-20 17:38:52 +0000

    made it so it uses my own version of strlen, strcpy

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include "dog.h"
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 /**
  *new_dog - initializes variable of type struct dog
@@ -17,30 +19,72 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *nowner;
 	char *nname;
 
-	nowner = "";
-	nname = "";
+	if (name == NULL)
+		return (NULL);
+	if (owner == NULL)
+		return (NULL);
+
 	newdog = malloc(sizeof(dog_t));
 	if (newdog == NULL)
 		return (NULL);
-	nname = strcpy(nname, name);
+	nname = malloc((_strlen(name) + 1));
 	if (nname == NULL)
 	{
 		free(newdog);
 		return (NULL);
 	}
-	nowner = strcpy(nowner, owner);
+	nowner = malloc((_strlen(owner) + 1));
 	if (nowner == NULL)
 	{
 		free(nname);
 		free(newdog);
 		return (NULL);
 	}
+	nname = _strcpy(nname, name);
+	nowner = _strcpy(nowner, owner);
 	newdog->name = nname;
 	newdog->owner = nowner;
 	newdog->age = age;
 
-	free(nname);
-	free(nowner);
-
 	return (newdog);
+}
+
+/**
+ *_strlen - funct that gives you the lentgh of an string
+ *@s: the string we pass
+ *Return: the number of times it looped
+ */
+int _strlen(char *s)
+{
+	int index = 0;
+
+	while (s[index] != ('\0'))
+	{
+		index++;
+	}
+	return (index);
+}
+
+/**
+ * *_strcpy - function
+ * @dest: parameter
+ * @src: parameter
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int idx, len = 0;
+
+	for (idx = 0 ; src[idx] != 0 ; idx++)
+	{
+		len++;
+	}
+
+	for (idx = 0 ; idx <= len ; idx++)
+	{
+		dest[idx] = src[idx];
+	}
+
+	return (dest);
 }
