9af8fd6dca92609a195a1e37e71b7a218e076277
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-21 18:48:47 +0000

    prints name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ *print_name - prints name
+ *@name: string
+ *@f: function that we are passing to print_name
+ *
+ */
+void print_name(char *name, void (*f)(char *c))
+{
+	if (f == NULL)
+		return;
+	f(name);
+}
