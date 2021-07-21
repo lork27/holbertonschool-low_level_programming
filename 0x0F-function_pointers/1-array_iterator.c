806dfcf3349387e6a80a4c6f4a79ebe1ebfc40c1
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-21 19:13:06 +0000

    executes function given as a parameter to each element of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- *array_iteractor - executes action function on each element of array
+ *array_iterator - executes action function on each element of array
  *@array: the array we parsed and execute
  *@size: size of the array
  *@action: function we pass to be executed on each element of the array
