1e139da6e6e0b7a492754bc95928de52b6a098ed
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-22 14:02:04 +0000

    repository for 0x10 variadic functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef _VARIADIC_FUNCTIONS_H
+#define _VARIADIC_FUNCTIONS_H
+
+int sum_them_all(conts unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#ifdef /*_VARIADIC_FUNCTIONS_H*/
