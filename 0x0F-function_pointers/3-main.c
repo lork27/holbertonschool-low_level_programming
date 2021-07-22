bd8d4d68cfbd212a2b42f0c049f48a8d2687c31d
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-22 00:43:31 +0000

    added test for divided by zero

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -25,6 +25,11 @@ int main(int argc, char *argv[])
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
+	if ((*argv[2] == '/' || *argv[2] == '%') && (num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	printf("%d\n", calc(num1, num2));
 	return (0);
 }
