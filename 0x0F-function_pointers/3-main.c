fc4a91cb1bf506782b248009b2b218f7fde7e466
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-22 00:45:59 +0000

    checking that the operator is a single character

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	calc = get_op_func(argv[2]);
-	if (calc == NULL)
+	if (calc == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
