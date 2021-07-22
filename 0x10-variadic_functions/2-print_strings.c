142a66868e548ff805ba51f68d20e641decd1f30
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-22 16:44:33 +0000

    checks if any string arg is null

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,20 +10,38 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
+	char *str;
 
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
+		str = va_arg(ap, char *);
+
 		if (i + 1 != n)
 		{
 			if (separator == NULL)
-				printf("%s", va_arg(ap, char *));
+			{
+				if (str != NULL)
+					printf("%s", str);
+				else
+					printf("(nil)");
+			}
 			else
-				printf("%s%s", va_arg(ap, char *), separator);
+			{
+				if (str != NULL)
+					printf("%s%s", str, separator);
+				else
+					printf("(nil)%s", separator);
+			}
 		}
 		else
-			printf("%s", va_arg(ap, char *));
+		{
+			if (str != NULL)
+				printf("%s", str);
+			else
+				printf("(nil)");
+		}
 	}
 	va_end(ap);
 	printf("\n");
