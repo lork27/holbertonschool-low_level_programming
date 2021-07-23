b94ae79a13c3a2cc95bd8658356a4e5483d230a4
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-23 03:12:34 +0000

    function that prints a list of types of arguments

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+/**
+ *print_all - print all given argument a la printf
+ *@format: list of types of arguments passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i;
+	char *c;
+	char *comma = "";
+	va_list list;
+
+	if (format == NULL)
+	{
+		printf("\n");
+		return;
+	}
+
+	va_start(list, format);
+	while (format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", comma, va_arg(list, int));
+				break;
+			case 'i':
+				printf("%s%i", comma, va_arg(list, int));
+				break;
+			case 'f':
+				printf("%s%f", comma, va_arg(list, double));
+				break;
+			case 's':
+				c = va_arg(list, char *);
+				if (c == NULL)
+					c = "(nil)";
+				else
+					printf("%s%s", comma, c);
+				break;
+		}
+		comma = ", ";
+		i++;
+	}
+	va_end(list);
+	printf("\n");
+}
