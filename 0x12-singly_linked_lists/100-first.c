c7987cb9d4d1cc2a1519c40b2084b1b64e2c5921
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-30 12:54:24 +0000

    executes before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *premain - runs before main()
+ *
+ */
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
