98676e5f303e23a2d90951fbe0ed9333bfc7c01a
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-30 02:53:29 +0000

    free every node in linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ *free_list - frees up every node of type list_t type
+ *@head: pointer to the first node of list_t
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
