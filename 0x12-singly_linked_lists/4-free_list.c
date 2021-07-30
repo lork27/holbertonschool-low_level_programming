eb307f6a7e88f1191e7c6d1b45f2997f85351b1e
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-30 03:05:58 +0000

    free every node in linked list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,11 +8,13 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-	while (head != NULL)
+	temp = head;
+
+	while (tmp != NULL)
 	{
-		temp = head;
-		head = head->next;
+		temp = temp->next;
+		free(head->str);
 		free(head);
-		head = temp;
 	}
+	free(temp);
 }
