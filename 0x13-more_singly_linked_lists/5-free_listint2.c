aea0038231b9658d868876f8b94ec6865e310209
lork27 <g.lorca.lamadrid@gmail.com>
2021-08-03 15:47:46 +0000

    reverted changes back to check if head is NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,10 +6,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = *head;
+	listint_t *temp;
 	listint_t *temp2;
+
+	if (*head == NULL)
+		return;
 	if (*head != NULL)
 	{
+		temp = *head;
+
 		while (temp != NULL)
 		{
 			temp2 = temp->next;
