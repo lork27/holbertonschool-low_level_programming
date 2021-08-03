114bbf9daf41972bbae5e8dab60a227cafbe4eeb
lork27 <g.lorca.lamadrid@gmail.com>
2021-08-03 18:55:51 +0000

    deletes head node and returns the int from that deleted node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ *pop_listint - deletes head node and returns n value of it
+ *@head: pointer to the pointer of the head node
+ *Return: n value inside head node
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int headint;
+
+	if (head == NULL)
+		return (0);
+	temp = *head;
+	*head = temp->next;
+	headint = temp->n;
+	free(temp);
+	return (headint);
+
+}
