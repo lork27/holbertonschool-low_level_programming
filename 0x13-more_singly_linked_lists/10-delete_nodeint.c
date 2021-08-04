f92357e4aeb9b9b038cb24dbf7e8825d634f7ad7
lork27 <g.lorca.lamadrid@gmail.com>
2021-08-04 20:17:06 +0000

    deletes node at given index of a linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		if (*head == NULL)
-			return (0);
+			return (-1);
 		*head = temp->next;
 		free(temp);
 		return (1);
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	for (i = 1; i < index; i++)
 	{
 		if (temp->next == NULL || temp == NULL)
-			return (0);
+			return (-1);
 		temp = temp->next;
 	}
 	delete = temp->next;
