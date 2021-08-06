85334dd5afeb52e57c3f7be931049f1526b6b9f8
lork27 <g.lorca.lamadrid@gmail.com>
2021-08-06 13:34:27 +0000

    return value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,7 +13,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index > sizeof(n) * 8)
 		return (-1);
 
-	for ( ; index > 0; index--)
+	for ( ; index => 0; index--)
 	{
 		bin = n >> index;
 		if (bin & 1)
