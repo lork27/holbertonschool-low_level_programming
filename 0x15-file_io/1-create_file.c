be9964d2ee019f5462855b73667d2efdf91006de
lork27 <g.lorca.lamadrid@gmail.com>
2021-08-10 03:51:18 +0000

    no comment

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,15 +9,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int o, r;
+	int fd;
+	int i = 0;
 
 	if (text_content == NULL)
 		return (-1);
-	o = open(filename, O_CREAT | O_RDWR, 0600);
-	r = read(o, text_content, strlen(text_content));
-	if (o == -1 || r == -1)
+	fd = open(filename, O_CREAT| O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
-	close(o);
+
+	if  (text_content != NULL)
+	{
+		while(text_content[i] != '\0')
+		{
+			i++;
+		}
+		write(fd, text_content, i);
+	}
+	close(fd);
 	return (1);
 
 }
