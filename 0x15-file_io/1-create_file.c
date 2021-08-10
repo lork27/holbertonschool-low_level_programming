5ea80bad1d215f8419200a7ba10a2568ed2217b6
lork27 <g.lorca.lamadrid@gmail.com>
2021-08-10 03:53:16 +0000

    bettyfied

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,13 +14,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT| O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
 	if  (text_content != NULL)
 	{
-		while(text_content[i] != '\0')
+		while (text_content[i] != '\0')
 		{
 			i++;
 		}
