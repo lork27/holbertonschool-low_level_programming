c6271f6f68cf4728bc5e129ffd4c5fc5a3677dc2
lork27 <g.lorca.lamadrid@gmail.com>
2021-08-10 16:28:33 +0000

    hello

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  *create_file - creates a file
  *@filename: name of the file to be created
- *@text_content: NULL terminated string to write to the file
+ *@text_content: NULL terminated string to write to the file1
  *Return: 1 success, -1 failure
  */
 
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	int fd, w;
 	int i = 0;
 
-	if (text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
 	{
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (1);
 	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
