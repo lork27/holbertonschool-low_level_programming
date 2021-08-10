854376674d79d17e3a2612cdaef012a4a6ebb987
lork27 <g.lorca.lamadrid@gmail.com>
2021-08-10 15:56:58 +0000

    fixed typo checking for no filename edgecase

--- 1-create_file.c
+++ 1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	int fd, w;
 	int i = 0;
 
-	if (text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
 	{
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (1);
 	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
