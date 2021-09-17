a4f47e132fb3a9d74735ab2a5bb59f1d1709ee43
lork27 <g.lorca.lamadrid@gmail.com>
2021-09-17 19:35:13 +0000

    ADDS node at start of dlinked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,15 +8,13 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = *head;
-	
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 	if (!*head)
 	{
 		new->n = n;
-		/*new->prev = NULL;
-		new->next = NULL;*/
 		*head = new;
 		return (new);
 	}
