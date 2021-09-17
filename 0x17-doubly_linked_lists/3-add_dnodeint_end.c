1c7ccf83eb34acd4c1cf7b71029d5b085be4757d
lork27 <g.lorca.lamadrid@gmail.com>
2021-09-17 20:26:01 +0000

    ADDS new node at the end of dlinkedlist

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ *add_dnodeint_end - add new node at end of dlinked list
+ *@head: pointer to dlinkedlist
+ *@n: node datta
+ *Return: created node
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new = *head;
+	dlistint_t *tmp = *head;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+
+	if (!*head)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = new;
+	tmp->prev = *head;
+	return (new);
+}
