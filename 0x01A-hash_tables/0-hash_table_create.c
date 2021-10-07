3ef72813c594000fdb7eb33bcd709582a0fd9175
lork27 <g.lorca.lamadrid@gmail.com>
2021-10-07 18:37:03 +0000

    fixed task 0

--- 0x01A-hash_tables/0-hash_table_create.c
+++ 0x01A-hash_tables/0-hash_table_create.c
@@ -8,18 +8,23 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new;
+	hash_table_t *hash_table;
 	unsigned long int idx;
 
 	if (size == 0)
 		return (NULL);
-	new = malloc(sizeof(hash_table_t));
-	if (new == NULL)
-		return (NULL);
-	new->size = size;
-	new->array = malloc(sizeof(hash_node_t) * size);
-	for (idx = 0; idx > size; idx++)
-		new->array[idx] = NULL;
 
-	return (new);
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
+
+	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_table->array) * size);
+	for (idx = 0; idx < size; idx++)
+	{
+		hash_table->array[idx] = NULL;
+	}
+
+
+	return (hash_table);
 }
