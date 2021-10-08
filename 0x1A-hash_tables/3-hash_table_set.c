f9cf9edd44b483a03d161a5919a1a63ed708e7c4
lork27 <g.lorca.lamadrid@gmail.com>
2021-10-08 20:53:23 +0000

    trying some edgecases but without typos this time

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
-	if (strlen(key) || strlen(value))
+	if (strlen(key) == 0 || strlen(value) == 0)
 		return (0);
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
